c958b2a8083c102b147976e32a35f198432c0a64
chalscc <carlescatalancalabuig@gmail.com>
2024-11-23 11:41:20 +0100

    Tree sitter textobjects bindings

--- lua/carles/plugins/treesitter.lua
+++ lua/carles/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	config = function()
 		-- import nvim-treesitter plugin
@@ -15,11 +16,54 @@ return {
 				enable = true,
 			},
 			-- enable indentation
-			indent = { enable = true },
+			indent = {
+				enable = true,
+			},
 			-- enable autotagging (w/ nvim-ts-autotag plugin)
 			autotag = {
 				enable = true,
 			},
+			-- default values
+			modules = {},
+			sync_install = false,
+			auto_install = false,
+			ignore_install = {},
+
+			textobjects = {
+				select = {
+					enable = true,
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = { query = "@function.outer", desc = "Function" },
+						["if"] = { query = "@function.inner", desc = "Function" },
+
+						["ac"] = { query = "@call.outer", desc = "Call" },
+						["ic"] = { query = "@call.inner", desc = "Call" },
+
+						["aa"] = { query = "@parameter.outer", desc = "Parameter" },
+						["ia"] = { query = "@parameter.inner", desc = "Parameter" },
+
+						["av"] = { query = "@assignment.lhs", desc = "Left assignment" },
+						["iv"] = { query = "@assignment.rhs", desc = "Right assignment" },
+
+						["al"] = { query = "@loop.outer", desc = "Loop" },
+						["il"] = { query = "@loop.inner", desc = "Loop" },
+
+						["ai"] = { query = "@conditional.outer", desc = "If" },
+						["ii"] = { query = "@conditional.inner", desc = "If" },
+
+						["ar"] = { query = "@return.outer", desc = "Return" },
+						["ir"] = { query = "@return.inner", desc = "Return" },
+
+						["a/"] = { query = "@comment.outer", desc = "Comment" },
+
+						["in"] = { query = "@number.inner", desc = "Number" },
+					},
+				},
+			},
+
 			-- ensure these language parsers are installed
 			ensure_installed = {
 				"json",
