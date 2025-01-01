0646fa21615d4811e01b95a793d67baa7aa68f37
chalscc <carlescatalancalabuig@gmail.com>
2025-01-01 13:28:32 +0100

    New custom function to switch between camelCase and snake_case. <lead>sc keybind

--- lua/carles/core/keymaps.lua
+++ lua/carles/core/keymaps.lua
@@ -21,3 +21,11 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- custom functions
+keymap.set(
+	"n",
+	"<Leader>sc",
+	'<cmd>lua require("carles.functions.switch-case").switch_case()<CR>',
+	{ desc = "Switch case" }
+)
