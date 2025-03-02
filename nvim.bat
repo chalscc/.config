d584fa76cfa134584a2bb5a77d3e8c0be4ad9671
chalscc <carlescatalancalabuig@gmail.com>
2025-03-02 01:06:04 +0100

    Moved entire configuration to %USERPROFILE%\.config folder

---
+++ nvim.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Add the following folder to the path environment variables
+REM %USERPROFILE%\.config
+
+REM Move the folder higher in the PATH to ensure this script is detected before the default nvim.exe
+
+set XDG_CONFIG_HOME=%USERPROFILE%\.config
+set XDG_DATA_HOME=%USERPROFILE%\.config
+set XDG_STATE_HOME=%USERPROFILE%\.config
+
+nvim.exe %*
