9029f9e83e3a31a5fed5e33090bbc678c3be384a
chalscc <carlescatalancalabuig@gmail.com>
2025-03-09 20:30:32 +0100

    Databse pluggin added

--- nvim/lua/carles/plugins/nvim-cmp.lua
+++ nvim/lua/carles/plugins/nvim-cmp.lua
@@ -49,6 +49,7 @@ return {
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
+				{ name = "vim-dadbod-completion" }, -- database queries completions (table/columns)
 			}),
 
 			-- configure lspkind for vs-code like pictograms in completion menu
